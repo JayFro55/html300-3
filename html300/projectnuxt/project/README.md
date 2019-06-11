2b5bcc3c32e1b222e11932e22e80d0c164523cc3
Jason DX <jxie7@uw.edu>
2019-06-11 06:28:11 -0700

    sdf

---
+++ html300/projectnuxt/project/README.md
@@ -0,0 +1,29 @@
+SEO stands for search engine optomization which is the relevancy of your page in a search engine. This is incredibly important because in this day and age people more often than not look things up using Google. Your pages traffic can be highly dependent on how highly your page is optomized. Therefore to improve SEO multiple things should be considered. The page must be indexed to be included in Google's search results. There should be many links in places. What also really helps is having articles written that include your pages name in the title.
+
+
+
+
+
+
+# project
+
+> My delightful Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ html300/projectnuxt/project/pages/About.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container">
+    
+    <nuxt-link to="/">Home</nuxt-link>
+    
+
+    <!-- {{ info }} -->
+
+    <h1>Bulbasaur Flow</h1>
+    
+  <div v-for="poke in info" class="poke">
+
+    <!-- {{ poke.name }}: -->
+    <span>
+        <span class="evos" v-html="poke.name"></span>
+    </span>
+  </div>
+
+  <v-carousel>
+    <v-carousel-item v-for="(item,i) in items" :key="i" :src="items.src"></v-carousel-item>
+  </v-carousel>
+  </div>
+  
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+
+
+export default {
+  
+  data () {
+    return {
+      info: null,
+      items: [
+        {
+          src: 'assets/bulbasaur.png'
+        },
+        {
+          src: '../assets/ivysaur.png'
+        },
+        {
+          src: 'venusaur.png'
+        }
+        
+      ]
+    }
+  },
+
+    
+
+  mounted () {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon?limit=3')
+      .then(response => (this.info = response.data.results))
+  }
+
+  
+
+}
+</script>
+
+<style>
+.evos {
+  margin: 5px;
+  font-size: 15pt;
+}
+
+body {
+    background-image: url('../assets/bulbasaur.png');
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
